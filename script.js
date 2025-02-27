c55c109a593e8d2f34aafb72dc78fb17ab3a196f
AidanP71 <marineforce171@gmail.com>
2025-02-27 09:15:14 -0500

    Button turned into a circle and centered comments added, need to display text on screen and not a alert.

--- script.js
+++ script.js
@@ -3,5 +3,6 @@ const button1 = document.getElementById("button1");
 button1.addEventListener("click", yell_at_user);
 
 function yell_at_user() {
+    // This will make a "notification pop up saying "STOP PRESSING THIS BUTTON!
     alert("STOP PRESSING THIS BUTTON!");
 }
