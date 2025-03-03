657ea254fb4c26da9194874a678c79b8e73d3382
AidanP71 <marineforce171@gmail.com>
2025-03-03 09:12:35 -0500

    added else if clicks need to fix my keyframes

--- script.js
+++ script.js
@@ -1,8 +1,80 @@
-const button1 = document.getElementById("button1");
+let text = document.getElementById("text");
+let button1 = document.getElementById("button1");
 
-button1.addEventListener("click", yell_at_user);
+let clicks = 0;
 
-function yell_at_user() {
-    // This will make a "notification pop up saying "STOP PRESSING THIS BUTTON!
-    alert("STOP PRESSING THIS BUTTON!");
+button1.addEventListener("click", annoy);
+
+function annoy() {
+    clicks++;
+    if(clicks == 1 ) {
+        text.innerHTML = "Hey! Stop That!";
+    }
+    else if( clicks == 2 ) {
+        text.innerHTML = "I said stop!"
+    }
+
+    else if (clicks == 3 ) {
+        text.innerHTML = "I'm pretty sure I said stop."
+    }
+    else if (clicks == 4 ) {
+        text.innerHTML = "You're still clicking?"
+    }
+    else if (clicks == 5 ) {
+        text.innerHTML = "Why?"
+    }
+    else if (clicks == 6 ) {
+        text.innerHTML = "Go outside"
+    }
+    else if (clicks == 7 ) {
+        text.innerHTML = "You're seriously still clicking?"
+    }
+    else if (clicks == 8 ) {
+        text.innerHTML = "I'm trying to go to sleep"
+    }
+    else if (clicks == 9 ) {
+        text.innerHTML = "Really? You're still clicking"
+    }
+    else if (clicks == 10) {
+        text.innerHTML = "Fine"
+    }
+    else if (clicks == 11) {
+        text.innerHTML = "I'm going to sleep"
+    }
+    else if (clicks == 12) {
+        text.innerHTML = "..."
+    }
+    else if (clicks == 13) {
+        text.innerHTML = "..."
+    }
+    else if (clicks == 14) {
+        text.innerHTML = "..."
+    }
+    else if (clicks == 15) {
+        text.innerHTML = "..."
+    }
+    else if (clicks == 16) {
+        text.innerHTML = "..."
+    }
+    else if (clicks == 17) {
+        text.innerHTML = "..."
+    }
+    else if (clicks == 18) {
+        text.innerHTML = "..."
+    }
+    else if (clicks == 19) {
+        text.innerHTML = "..."
+    }
+    else if (clicks == 20) {
+        text.innerHTML = "..."
+    }
+    else if (clicks == 21) {
+        text.innerHTML = "*wakes up*"
+    }
+    else if (clicks == 22) {
+        text.innerHTML = "You are still here?"
+    }
+    else if (clicks == 23) {
+        text.innerHTML = "Fine, try and click me now."
+    }
 }
